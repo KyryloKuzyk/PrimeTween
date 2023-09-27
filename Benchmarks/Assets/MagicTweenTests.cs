109184d13fe34be8cab143dff58b9dcfc1981078
Kyrylo Kuzyk <>
2023-09-27 13:01:54 +0200

    Fix test name.

--- Benchmarks/Assets/MagicTweenTests.cs
+++ Benchmarks/Assets/MagicTweenTests.cs
@@ -31,7 +31,7 @@ public class MagicTweenTests {
     const float longDuration = 10f;
     [UnityTest, Performance] public IEnumerator _01_PositionAnimation_MagicTween() => DOTween_PrimeTweenTests.measureAverageFrameTimes(() => transform.TweenPosition(endValue, longDuration));
     readonly Quaternion targetRotation = Quaternion.Euler(0,0,10);
-    [UnityTest, Performance] public IEnumerator _01_RotationAnimation_DOTween() => DOTween_PrimeTweenTests.measureAverageFrameTimes(() => transform.TweenRotation(targetRotation, longDuration));
+    [UnityTest, Performance] public IEnumerator _01_RotationAnimation_MagicTween() => DOTween_PrimeTweenTests.measureAverageFrameTimes(() => transform.TweenRotation(targetRotation, longDuration));
     const float shortDuration = 0.0001f;
     [UnityTest, Performance] public IEnumerator _07_Animation_Start_MagicTween() => DOTween_PrimeTweenTests.measureFrameTime(() => transform.TweenPosition(endValue, shortDuration));
     [Test, Performance] public void _05_Animation_GCAlloc_MagicTween() => DOTween_PrimeTweenTests.measureGCAlloc(() => transform.TweenPosition(endValue, shortDuration));
